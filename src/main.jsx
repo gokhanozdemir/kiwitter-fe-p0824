635bd0371000a855500713e787f70e98cd4c5a76
gokhanozdemir <gn.ozdemir@gmail.com>
2025-01-15 21:12:12 +0300

    Add React Query for data fetching and update main rendering structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,20 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import UserProvider from "./contexts/UserContext";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </QueryClientProvider>
   </BrowserRouter>
 );
